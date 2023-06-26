964e851e0f9740b21ed8e885ae66211d7507a2ab
bahp <bernard.hernandez.perez@gmail.com>
2023-06-26 18:52:07 +0100

    ~ generate timeseries

--- examples/utils/plot_generate_data_AST.py
+++ examples/utils/plot_generate_data_AST.py
@@ -7,6 +7,7 @@ Generate data AST
 # Libraries
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # -----------------------------------------------------------
@@ -14,17 +15,150 @@ import matplotlib.pyplot as plt
 # -----------------------------------------------------------
 CONFIGURATION = [
     {
-        'specimen': 'BLDCUL',
-        'microorganism': 'ORG1',
-        'antmicrobial': 'ABX1',
-        #'range': [2010, 2020],
-        #'kwargs':
+        'date': ['2010-01-01', '2018-01-05'],
+        'info': {
+            'SPECIMEN': 'BLDCUL',
+            'MICROORGANISM': 'SAUR',
+            'ANTIMICROBIAL': 'ATAZ',
+        },
+        'kwargs': {
+            'line_offset': 80,
+            'line_slope': -0.3,
+            'sin_amplitude': 5
+        }
     },
-
+    {
+        'date': ['2010-01-01', '2018-01-05'],
+        'info': {
+            'SPECIMEN': 'BLDCUL',
+            'MICROORGANISM': 'SAUR',
+            'ANTIMICROBIAL': 'ACIP',
+        },
+    },
+    {
+        'date': ['2010-05-01', '2019-01-05'],
+        'info': {
+            'SPECIMEN': 'BLDCUL',
+            'MICROORGANISM': 'ECOL',
+            'ANTIMICROBIAL': 'AMER',
+        },
+        'kwargs': {
+            'line_offset': 80,
+            'line_slope': -0.01,
+            'sin_amplitude': 1,
+            'noise_amplitude': 1
+        }
+    },
+    {
+        'date': ['2010-05-01', '2019-01-05'],
+        'info': {
+            'SPECIMEN': 'URICUL',
+            'MICROORGANISM': 'ECOL',
+            'ANTIMICROBIAL': 'AMER',
+        },
+        'kwargs': {
+            'line_offset': 10,
+            'line_slope': 0.2,
+            'sin_amplitude': 3
+        }
+    },
+    {
+        'date': ['2010-05-01', '2018-01-05'],
+        'info': {
+            'SPECIMEN': 'URICUL',
+            'MICROORGANISM': 'ECOL',
+            'ANTIMICROBIAL': 'ACIP',
+        },
+        'kwargs': {
+            'sin_amplitude': 3,
+            'sin_stretch': 3
+        }
+    },
+    {
+        'date': ['2010-05-01', '2020-01-05'],
+        'info': {
+            'SPECIMEN': 'URICUL',
+            'MICROORGANISM': 'ECOL',
+            'ANTIMICROBIAL': 'ATAZ',
+        },
+        'kwargs': {
+            'line_offset': 80,
+            'line_slope': 0.3,
+            'sin_amplitude': 0,
+            'jump_start': 30,
+            'jump_amplitude': 5
+        }
+    },
+    {
+        'date': ['2012-05-01', '2018-01-05'],
+        'info': {
+            'SPECIMEN': 'URICUL',
+            'MICROORGANISM': 'SAUR',
+            'ANTIMICROBIAL': 'AMER',
+        },
+        'kwargs': {
+            'line_offset': 20,
+            'line_slope': 0.1,
+            'sin_amplitude': 0,
+            'noise_amplitude': 0
+        }
+    },
 ]
 
+def make_series(n_samples,
+                line_slope=0.2,
+                line_offset=30,
+                sin_amplitude=1,
+                sin_stretch=1,
+                sin_c=1,
+                noise_amplitude=2,
+                random=False,
+                jump_start=None,
+                jump_amplitude=5):
+    """Create series...
 
-def create_ast_from_values(y, ns=None):
+    f(x) = a * sin( b (x+c)) + d
+
+    """
+    # Configuration
+    if random:
+        line_slope = np.random.randint(10, 20) / 100
+        line_offset = np.random.randint(40, 60)
+        sin_amplitude = np.random.randint(1, 60) / 100
+        sin_stretch = np.random.randint(3, 10)
+        sin_c = np.random.randint(5, 10)
+        noise_amplitude = np.random.randint(10, 20)
+        jump_start = np.random.randint(0, 2)
+        jump_amplitude = np.random.randint(5, 10)
+
+    # Create components and final function
+    x = np.linspace(0, 50, n_samples)
+    line = (line_slope * x) + line_offset
+    noise = noise_amplitude * np.random.rand(len(x))
+    season = sin_amplitude * np.sin(sin_stretch * x)
+    y = line + season + noise + x/5
+
+    # Add a jump.
+    if jump_start is not None:
+        y[jump_start:] += jump_amplitude
+
+    # .. note: This is done to avoid having extreme
+    #          values which end up giving a negative
+    #          number of R or S. In this case it will
+    #          have the same shape but will be using all
+    #          the y-axis from 0.2 to 0.8
+
+    # Normalize within the range (20, 80)
+    rmin, rmax = 20, 80
+    if (max(y) > rmax) or (min(y) < rmin):
+        y = ((y - min(y)) / (max(y) - min(y)))
+        y = y * (rmax - rmin) + rmin
+
+    # Return
+    return y
+
+
+def create_ast_from_values(cfg, nr=30): #y, dates, info, nr=None):
     """Create AST records.
 
     .. note:: The values inputed must be between 0 and 100.
@@ -39,117 +173,59 @@ def create_ast_from_values(y, ns=None):
     Returns
     -------
     """
+    # Generate dates
+    sdate = cfg.get('date')[0]
+    edate = cfg.get('date')[1]
+    dates = pd.date_range(sdate, edate, freq='m')
 
-    # Create number of positive.
-    if ns is None:
-        ns = np.ones(shape=(1, y.shape[0])).astype(int) * 100
-    if isinstance(ns, list):
-        ns = np.array(ns)
+    # Generate y function
+    kwargs = cfg.get('kwargs', {})
+    random = not bool(kwargs)
+    y = make_series(dates.shape[0], random=random, **kwargs)
 
-    # Compute number of negatives.
-    nr = ((ns*y) / (1-y)).round(decimals=0).astype(int)
+    if nr is None:
+       nr = np.random.randint(10, 100, size=y.shape[0])
 
-    # Address extreme cases
-    print(nr)
-    print(ns)
-    print(sari)
+    # Compute number of S based on number of R.
+    ns = (((100/y) - 1) * nr).astype(int)
 
-    import sys
-    sys.exit()
-    return nr.flatten(), ns.flatten()
+    # Create DataFrame
+    data = pd.DataFrame(data=dates, columns=['DATE'])
 
+    # Fill rest of information
+    for k, v in cfg.get('info').items():
+        data[k] = v
+    data['ns'] = ns
+    data['nr'] = nr
 
+    # Create records by repeating entries and
+    # concatenating the results
+    r = data \
+        .loc[data.index.repeat(data.nr)] \
+        .assign(SENSITIVITY='resistant') \
+        .reset_index(drop=True)
 
+    s = data \
+        .loc[data.index.repeat(data.ns)] \
+        .assign(SENSITIVITY='sensitive') \
+        .reset_index(drop=True)
 
-def create_ast_from_xy(x, y, info):
-    """
-    Create AST records matching R/R+S = y
-
-    :param x:
-    :param y:
-    :param info:
-    :return:
-    """
-
-
-
-    # Get number of resistance records (ints)
-    #nr = np.random.randint(1, 10, size=(1, x.shape[0]))
-    #nr = np.ones(shape=(1, x.shape[0])).astype(int) * 500
-    #ns = np.round((nr / y) - nr).astype(int)
-    ns = np.ones(shape=(1, x.shape[0])).astype(int) + 2000
-    nr = (ns / y).astype(int)
-    #nr = ((y*ns) / (1-y)).astype(int)
-
-    print('nr', nr)
-    print('ns', ns)
-
-    # Create model record
-    v = list(info.values())
-    r = [[i, 'resistant'] + v for i in x]
-    s = [[i, 'sensitive'] + v for i in x]
-
-    print(nr)
-    print(ns)
-
-    # Repeat n number of times
-    r = np.repeat(r, nr.flatten(), axis=0)
-    s = np.repeat(s, ns.flatten(), axis=0)
+    records = pd.concat([r, s], axis=0)
 
     # Return
-    return np.vstack((r,s))
+    return records
 
 
-def straight_line(x, slope, intercept):
-    ys = intercept + slope * x
-    return x, ys
+# Constants
+SPECIMENS = ['BLDCUL', 'URICUL']
+MICROORGANISMS = ['ECOL', 'KPNE']
+ANTIMICROBIALS = ['ATAZ', 'AMER', 'ACIP']
 
 
-def a():
-    pass
-
-
-# Get dates between range
-sdate = '2010-01-01'
-edate = '2010-01-05'
-d = pd.date_range(sdate, edate, freq='d')
-
-# Compute sari function
-x = np.arange(d.shape[0])
-sari = x + 10
-
-print("sari")
-print(sari)
-
-# Define sari values
-#sari = np.array([0.09, 0.5, 0.57, 0.6, 0.57])
-
-# Compute number of resistant and sensitive records
-nr, ns = create_ast_from_values(sari, ns=sari)
-
-# Create DataFrame
-data = pd.DataFrame()
-data['DATE'] = d
-data['SPECIMEN'] = 's'
-data['MICROORGANISM'] = 'o'
-data['ANTIMICROBIAL'] = 'a'
-data['ns'] = ns
-data['nr'] = nr
-
-# Create records by repeating entries and
-# concatenating the results
-r = data \
-    .loc[data.index.repeat(data.nr)] \
-    .assign(SENSITIVITY='resistant') \
-    .reset_index(drop=True)
-
-s = data \
-    .loc[data.index.repeat(data.ns)] \
-    .assign(SENSITIVITY='sensitive') \
-    .reset_index(drop=True)
-
-
-records = pd.concat([r, s], axis=0)
+# Compute records
+records = pd.concat([
+    create_ast_from_values(cfg)
+        for cfg in CONFIGURATION])
 
 
 # ----------------------------
@@ -170,86 +246,20 @@ iti = iti.reset_index()
 
 # Show results
 print("Data:")
-
-plt.plot(sari)
-plt.figure()
-plt.plot(iti.index.values, iti.sari)
-plt.show()
-
-print(records)
-import sys
-sys.exit()
-
-
-
-
-
-
-
-
-
-
-
-
-# Create
-x = np.arange(d.shape[0])
-
-
-#y1 = np.sin(2*x)
-#mu, sigma = 0, 0.1
-#y2 = x + np.random.normal(mu, sigma)
-x1, y1 = x, np.sin(x) + 50
-x3, y3 = straight_line(x, 0.5, 0.2)
-y4 = np.array([10, 12, 24, 35, 48])
-
-print(y3)
-print(y4)
-
-print(x3.shape, y3.shape)
-print(d.shape, x.shape)
-
-
-
-data = create_ast_from_xy(d, y1, CONFIGURATION[0])
-
-# Create DataFrame
-data = pd.DataFrame(data,
-    columns=['DATE',
-             'SENSITIVITY',
-             'SPECIMEN',
-             'MICROORGANISM',
-             'ANTIMICROBIAL'])
-#data['DATE'] = data.X.map(dict(zip(x,d.values)))
-
-# Ensure is a datetime series.
-data.DATE = pd.to_datetime(data.DATE)
-
-# Show
-print("\nData:")
-print(data)
-print("\nTypes")
-print(data.dtypes)
-
-# ----------------------------
-# Compute SARI
-# ----------------------------
-# Show
-from pyamr.core.sari import SARI
-
-# Configuration
-shift, period = '1D', '1D'
-
-# Compute the index
-iti = SARI().compute(data, shift=shift,
-    period=period, cdate='DATE')
-
 print(iti)
 
-# Display
-iti = iti.reset_index()
+# Initialize a grid of plots with an Axes for each walk
+grid = sns.FacetGrid(iti, row="ANTIMICROBIAL",
+    hue="SPECIMEN", col='MICROORGANISM',
+    palette="tab20c", height=2, aspect=3)
 
-plt.plot(x, y1)
-plt.figure()
-plt.plot(iti.index.values, iti.sari)
+# Draw a horizontal line to show the starting point
+grid.refline(y=0.5, linewidth=0.75)
 
+# Draw a line plot to show the trajectory of each random walk
+grid.map(plt.plot, "DATE", "sari",
+    linewidth=0.75, marker="o", markersize=1)
+
+# Show
+plt.tight_layout()
 plt.show()
